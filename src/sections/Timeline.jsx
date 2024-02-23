a20d53dd26e4689c328c04ef5bfcb1bfaa50f0b1
arpan21020 <arpan21020@iiitd.ac.in>
2024-02-24 01:08:30 +0530

    timeline fixed

--- src/sections/Timeline.jsx
+++ src/sections/Timeline.jsx
@@ -1,9 +1,32 @@
+
+import React, { useEffect, useState } from 'react';
 const Timeline = () =>{
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [imageSrc, setImageSrc] = useState("/assets/timeline.svg");
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (windowWidth <= 744) {
+            setImageSrc("/assets/timeline2.svg");
+        } else {
+            setImageSrc("/assets/timeline.svg");
+        }
+    }, [windowWidth]);
     return (
         <section  id="timeline" className="flex flex-col justify-center w-full max-container">
             <h1 className='heading text-transparent text-stroke'>&lt;Timeline&gt;</h1>
-            <img src="/assets/timeline.png" className="" alt="" />
-            
+                <img src={imageSrc} className="" alt="" />
         </section>
     )
 }
